9af7f540238d575b8e493b3519d63e6640564148
Vardan Petrosyan <petrosyanvardan2009@gmail.com>
2024-06-19 21:11:57 +0400

    Deployed 62f2266 with MkDocs version: 1.6.0

---
+++ overrides/eiger-highlight.js
@@ -0,0 +1,13 @@
+var script1 = document.createElement('script');
+script1.src = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js';
+script1.defer = true;
+script1.onload = function() {
+  var script2 = document.createElement('script');
+  script2.src = '/overrides/eiger.js';
+  script2.defer = true;
+  script2.onload = function() {
+    hljs.highlightAll();
+  };
+  document.head.appendChild(script2);
+};
+document.head.appendChild(script1);
